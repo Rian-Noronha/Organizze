e88b23a54d15dff767e91618935ec413a6eca879
Rian-Noronha <riannoronha011@gmail.com>
2023-03-22 14:45:34 -0300

    Adding Slider.

--- app/src/main/java/com/rn/organizze/MainActivity.java
+++ app/src/main/java/com/rn/organizze/MainActivity.java
@@ -1,14 +1,53 @@
 package com.rn.organizze;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonNextVisible(false);
+        setButtonBackVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+
+                .background(R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+
+        );
+
+        addSlide(new FragmentSlide.Builder()
+
+                .background(R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+
+        );
+
+        addSlide(new FragmentSlide.Builder()
+
+                .background(R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+
+        );
+
+        addSlide(new FragmentSlide.Builder()
+
+                .background(R.color.white)
+                .fragment(R.layout.intro_4)
+                .canGoForward(false)
+                .build()
+        );
     }
 }
